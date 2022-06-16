518f1e8a7a7f4780172a09299351a21cefa78a47
gpavlikov <gpavlikov7@gmail.ru>
2022-06-16 21:03:09 +0500

    Сделан удобный ввод

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+
+
 from parser.censys import CensysParser
 
 
 if __name__ == '__main__':
+
+    ip = input('Введите айпи в формате x.x.x.x')
+
+    os.environ['CENSYS_API_URL'] = 'https://search.censys.io/hosts/' + ip
 
     parser = CensysParser()
 
